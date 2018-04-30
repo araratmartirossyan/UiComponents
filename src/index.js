a6c27de086cf5e15d9b72a802b3364e0528bfada
myfailemtions <myfailemtions@gmail.com>
2018-04-30 23:21:27 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,3 @@
 import Button from './components/Button'
 
-const components = {
-  Button
-}
-
-const install = Vue => {
-  Object.keys(components).forEach(key =>
-    Vue.component(key, components[key]))
-}
-
-// auto install
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
-}
-
-const API = {
-  install,
-  ...components
-}
-
-module.exports.default = module.exports = API // eslint-disable-line no-undef
+export default Button
--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
-import Vue from 'vue'
-import App from './App'
+import Button from './components/Button'
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+export default Button
